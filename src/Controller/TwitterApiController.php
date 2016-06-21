8652f21a5e08ee8ea1ebd7098f6ea5943888a4fa
heykarthikwithu <heykarthikwithu@gmail.com>
2016-06-21 18:52:05 +0530

    Progress commit - reads auth from the hybrid instance.

--- src/Controller/TwitterApiController.php
+++ src/Controller/TwitterApiController.php
@@ -10,6 +10,7 @@ namespace Drupal\twitter_api\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\twitter_api\Twitter\TwitterUsers;
 use Drupal\twitter_api\TwitterApiOAuth;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Class TwitterApiController.
@@ -28,6 +29,10 @@ class TwitterApiController extends ControllerBase {
     // Initialize session data.
     //session_start();
 
+    if ($hybridauth = hybridauth_get_instance()) {
+      return $this->_hybridauth_window_auth($hybridauth, 'Twitter');
+    }
+
     return [
       '#type' => 'markup',
       '#markup' => $this->t('Implement process method..!')
@@ -39,6 +44,30 @@ class TwitterApiController extends ControllerBase {
       '#type' => 'markup',
       '#markup' => $this->t('Implement process method..!')
     ];
+  }
+
+  public function endpoint() {
+    /*return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Implement process method..!')
+    ];*/
+    //return MENU_ACCESS_DENIED;
+    return new AccessDeniedHttpException();
+  }
+
+  public function _hybridauth_window_auth(\Hybrid_Auth $hybridauth, $provider_id) {
+    $params = array(
+      'hauth_return_to' => 'http://127.0.0.1/d82/twitter-api/process',
+    );
+
+    /*$provider = new \Hybrid_Provider_Adapter();
+    $provider->factory($provider_id, $params);*/
+    //$aa = \Hybrid_Auth::getAdapter($provider_id);
+    // return $aa;
+
+    $adapter = $hybridauth->authenticate($provider_id, $params);
+    $profile = (array) ($adapter->getUserProfile());
+    $a = '';
   }
 
 }
